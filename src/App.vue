902cb0f4fc8d11dad75642cfa556f6849d845b8e
Joe T. Wilson <jtwilson.home@gmail.com>
2021-11-09 22:13:19 -0700

    scoped App styles

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,13 @@
 <template>
-    <MainLayout>
-        <transition
-            name="content"
-            appear
-            appear-active-class="content-appear-enter-active"
-        >
-            <router-view class="mx-4" />
-        </transition>
-    </MainLayout>
+  <MainLayout>
+    <transition
+      name="content"
+      appear
+      appear-active-class="content-appear-enter-active"
+    >
+      <router-view class="mx-4" />
+    </transition>
+  </MainLayout>
 </template>
 
 <static-query>
@@ -22,36 +22,36 @@ query {
 <script>
 import MainLayout from "~/layouts/Default.vue";
 export default {
-    components: { MainLayout },
+  components: { MainLayout },
 
-    metaInfo() {
-        return {
-            title: this.$static.metadata.siteName,
-            meta: [
-                {
-                    key: "description",
-                    name: "description",
-                    content: this.$static.metadata.siteDescription,
-                },
-            ],
-        };
-    },
+  metaInfo() {
+    return {
+      title: this.$static.metadata.siteName,
+      meta: [
+        {
+          key: "description",
+          name: "description",
+          content: this.$static.metadata.siteDescription,
+        },
+      ],
+    };
+  },
 };
 </script>
 
-<style>
+<style scoped>
 .content-enter-active {
-    transition: all 1.5s cubic-bezier(0.19, 1, 0.22, 1) 1s;
+  transition: all 1.5s cubic-bezier(0.19, 1, 0.22, 1) 1s;
 }
 .content-leave-active {
-    transition: all 1s cubic-bezier(0.19, 1, 0.22, 1) 0s;
+  transition: all 1s cubic-bezier(0.19, 1, 0.22, 1) 0s;
 }
 .content-appear-enter-active {
-    transition: all 1.5s cubic-bezier(0.19, 1, 0.22, 1) 3s;
+  transition: all 1.5s cubic-bezier(0.19, 1, 0.22, 1) 3s;
 }
 .content-enter,
 .content-leave-to {
-    opacity: 0;
-    transform: translateY(20px);
+  opacity: 0;
+  transform: translateY(20px);
 }
 </style>
