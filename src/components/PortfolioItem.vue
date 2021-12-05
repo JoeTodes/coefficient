6a9de5f8d17e392b41a9a2db48752af5f8aa0a5a
JoeTodes <jtwilson.home@gmail.com>
2021-12-04 21:19:33 -0700

    alt on images

--- src/components/PortfolioItem.vue
+++ src/components/PortfolioItem.vue
@@ -1,7 +1,7 @@
 <template>
-  <div class="mx-8 sm:mx-4 my-4">
-    <a
-      class="
+    <div class="mx-8 sm:mx-4 my-4">
+        <a
+            class="
                 shadow-md
                 hover:shadow-lg
                 bg-gray-100
@@ -13,12 +13,12 @@
                 text-center
                 sm:text-left
             "
-      :href="entry.link"
-      target="_blank"
-      rel="noopener noreferrer"
-    >
-      <g-image
-        class="
+            :href="entry.link"
+            target="_blank"
+            rel="noopener noreferrer"
+        >
+            <g-image
+                class="
                     w-full
                     sm:h-full sm:w-40
                     shadow-md
@@ -27,23 +27,24 @@
                     mb-4
                     sm:mb-0
                 "
-        :src="entry.thumbnail"
-        onerror="this.src='/uploads/placeholder-square.jpg'"
-      />
-      <div class="flex flex-col h-full px-2 font-display justify-between">
-        <div class="font-bold text-lg underline">
-          {{ entry.title }}
-        </div>
-        <div class="overflow-hidden">{{ entry.description }}</div>
-      </div>
-    </a>
-  </div>
+                :src="entry.thumbnail"
+                :alt="entry.title"
+                onerror="this.src='/uploads/placeholder-square.jpg'"
+            />
+            <div class="flex flex-col h-full px-2 font-display justify-between">
+                <div class="font-bold text-lg underline">
+                    {{ entry.title }}
+                </div>
+                <div class="overflow-hidden">{{ entry.description }}</div>
+            </div>
+        </a>
+    </div>
 </template>
 
 <script>
 export default {
-  props: {
-    entry: {},
-  },
+    props: {
+        entry: {},
+    },
 };
 </script>
